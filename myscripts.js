cf46bc8f4c3eba5fcc557a5e817ee9358e9572a6
lekkrit <39566937+lekkrit@users.noreply.github.com>
2020-11-15 20:11:16 +0700

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,18 @@
+function result() {
+  document.getElementById('calcResult').value = eval( document.getElementById('calcResult').value);
+}
+
+
+function clearAll() {
+
+  document.getElementById('calcResult').value = "";
+}
+
+function mathsqrt(){
+  inp.value=Math.sqrt(inp.value)
+}
+
+var inp = document.getElementById('calcResult'); function addNumbers(num){  
+  inp.value=inp.value + num }
+
+
